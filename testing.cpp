4af80393f7172554c4cd12ef2b013914a2ed1015
lina-uwase <u.lina250@gmail.com>
2023-04-19 08:02:15 +0200

    initial commit

---
+++ testing.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+struct Student{
+    int age;
+    string name;
+    int rollNumber;
+}student1,student2,student3;
+
+void print_studentData(Student s){
+    cout<<s.age<<endl;
+    cout<<s.name<<endl;
+    cout<<s.rollNumber<<endl;
+}
+Student make
+
+int main(){
+    cout<<"enter name: ";
+    cin>>student1.name;
+    cout<<"enter age: ";
+    cin>>student1.age;
+    cout<<"enter rollNumber: ";
+    cin>>student1.rollNumber;
+
+    cout<<"Displaying data "<<endl;
+    print_studentData(student1);
+   
+    return 1;
+}
